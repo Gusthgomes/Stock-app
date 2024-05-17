60cde6a527bc13c1f1481d54598ade69d468be0c
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-05-17 12:01:16 -0300

    feat: created signIn and signUp methods

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  loginCard = true;
 
-  constructor() { }
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
 
-  ngOnInit() {
+  registerForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmitLoginForm(): void {
+    console.log('Dados recebidos: ', this.loginForm.value);
   }
 
+  onSubmitRegisterForm(): void {
+    console.log('Dados recebidos: ', this.registerForm.value);
+  }
 }
