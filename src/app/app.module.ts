6c4bcb07475918219378fe70f088595d6ded6da6
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-05-17 11:06:38 -0300

    chore: declaration modules and export components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './modules/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
   ],
+  exports: [HomeComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
